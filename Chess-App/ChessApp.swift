3e46c1182eda936f0e376250dfcabe4aa7dda3df
AJ <alexanderjessediaz@gmail.com>
2021-02-17 12:55:52 -0700

    Initial Commit

---
+++ Chess-App/ChessApp.swift
@@ -0,0 +1,20 @@
+//
+//  ChessApp.swift
+//  Chess-App
+//
+//  Created by AJ Diaz on 2/17/21.
+//
+
+import SwiftUI
+
+struct ChessApp: View {
+    var body: some View {
+        ContentView()
+    }
+}
+
+struct ChessApp_Previews: PreviewProvider {
+    static var previews: some View {
+        ChessApp()
+    }
+}
---
+++ Chess-App/Classes/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chess-App
+//
+//  Created by AJ Diaz on 2/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+    }
+
+
+}
+
